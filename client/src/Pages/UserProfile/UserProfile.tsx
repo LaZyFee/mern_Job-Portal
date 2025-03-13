e98109a194e445b7091149d2363a19b0f2d2af30
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-03-13 23:39:36 +0600

    userProfile page Created

--- client/src/Pages/UserProfile/UserProfile.tsx
+++ client/src/Pages/UserProfile/UserProfile.tsx
@@ -1,11 +1,25 @@
-import { Button, Divider } from "@mantine/core"
+import { ActionIcon, Divider } from "@mantine/core"
 import BannerImage from "/assets/Profile/banner.jpg"
 import AvatarImage from "/assets/avatar.png"
-import { IconBriefcase, IconMapPin } from "@tabler/icons-react"
+import { IconBriefcase, IconDeviceFloppy, IconMapPin, IconPencil } from "@tabler/icons-react"
 import { ExpCard } from "./ExpCard"
 import { CertificationCard } from "./CertificationCard"
+import { useState } from "react"
+import { SelectInput } from "../Post Job/SelectInput"
+import fields from "../../assets/Data/Profile"
 
 export const UserProfile = (props: any) => {
+    const select = fields;
+    const [edit, setEdit] = useState([false, false, false, false, false,])
+
+    const handleEdit = (index: number) => {
+        const newEdit = [...edit];
+        newEdit[index] = !newEdit[index];
+        setEdit(newEdit);
+    }
+
+
+
     return (
         <div className="w-3/4 mx-auto">
             <div className="relative">
@@ -15,22 +29,47 @@ export const UserProfile = (props: any) => {
             <div className="px-3 mt-16">
                 <div className="text-3xl font-semibold flex justify-between">
                     {props.name}
-                    <Button color="brightSun.4" variant="light">Message</Button>
+                    <ActionIcon variant="subtle" size="lg" radius="md" color="brightSun.4" onClick={() => handleEdit(0)}>
+                        {edit[0] ? <IconDeviceFloppy className="w-4/5 h-4/5" stroke={1.5} /> : <IconPencil className="w-4/5 h-4/5" stroke={1.5} />}
+                    </ActionIcon>
                 </div >
-                <div className="text-lg flex gap-1 items-center">
-                    <IconBriefcase className="h-4 w-4" stroke={1.5} /> {props.role} &bull; {props.company} </div>
-                <div className="text-md flex gap-1 items-center text-mine-shaft-300">
-                    <IconMapPin className="h-4 w-4" stroke={1.5} /> {props.location}
-                </div>
+                {
+                    edit[0] ? <>
+                        <div className="grid grid-cols-2 gap-5">
+                            <SelectInput {...select[0]} />
+                            <SelectInput {...select[1]} />
+                            <SelectInput {...select[2]} />
+                        </div>
+                    </>
+                        :
+                        <>
+                            <div className="text-lg flex gap-1 items-center">
+                                <IconBriefcase className="h-4 w-4" stroke={1.5} /> {props.role} &bull; {props.company} </div>
+                            <div className="text-md flex gap-1 items-center text-mine-shaft-300">
+                                <IconMapPin className="h-4 w-4" stroke={1.5} /> {props.location}
+                            </div>
+                        </>
+                }
+
+
             </div>
             <Divider my="md" />
             <div className="p-3">
-                <div className="text-2xl font-semibold mb-3">About </div>
+                <div className="text-2xl font-semibold mb-3 flex justify-between">
+                    About
+                    <ActionIcon variant="subtle" size="lg" radius="md" color="brightSun.4" onClick={() => handleEdit(1)}>
+                        {edit[1] ? <IconDeviceFloppy className="w-4/5 h-4/5" stroke={1.5} /> : <IconPencil className="w-4/5 h-4/5" stroke={1.5} />}
+                    </ActionIcon>
+                </div>
                 <div className="text-sm text-justify text-mine-shaft-300">{props.about}</div>
             </div>
             <Divider my="md" />
             <div className="p-3">
-                <div className="text-2xl font-semibold mb-3">Skills </div>
+                <div className="text-2xl font-semibold mb-3 flex justify-between">Skills
+                    <ActionIcon variant="subtle" size="lg" radius="md" color="brightSun.4" onClick={() => handleEdit(1)}>
+                        {edit[1] ? <IconDeviceFloppy className="w-4/5 h-4/5" stroke={1.5} /> : <IconPencil className="w-4/5 h-4/5" stroke={1.5} />}
+                    </ActionIcon>
+                </div>
                 <div className="flex flex-wrap gap-3">
                     {
                         props.skills.map((skill: string, index: number) =>
@@ -43,7 +82,11 @@ export const UserProfile = (props: any) => {
             </div>
             <Divider my="md" />
             <div className="p-3">
-                <div className="text-2xl font-semibold mb-3">Experiences</div>
+                <div className="text-2xl font-semibold mb-3 flex justify-between">Experiences
+                    <ActionIcon variant="subtle" size="lg" radius="md" color="brightSun.4" onClick={() => handleEdit(3)}>
+                        {edit[3] ? <IconDeviceFloppy className="w-4/5 h-4/5" stroke={1.5} /> : <IconPencil className="w-4/5 h-4/5" stroke={1.5} />}
+                    </ActionIcon>
+                </div>
             </div>
             <div className="flex flex-col gap-8">
                 {
@@ -54,7 +97,11 @@ export const UserProfile = (props: any) => {
             </div>
             <Divider my="md" />
             <div className="p-3">
-                <div className="text-2xl font-semibold mb-3">Certifications</div>
+                <div className="text-2xl font-semibold mb-3">Certifications
+                    <ActionIcon variant="subtle" size="lg" radius="md" color="brightSun.4" onClick={() => handleEdit(4)}>
+                        {edit[4] ? <IconDeviceFloppy className="w-4/5 h-4/5" stroke={1.5} /> : <IconPencil className="w-4/5 h-4/5" stroke={1.5} />}
+                    </ActionIcon>
+                </div>
             </div>
             <div>
                 {
